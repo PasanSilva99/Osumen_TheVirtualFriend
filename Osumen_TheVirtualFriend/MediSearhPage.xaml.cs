44b36c93af3e2642cb06daeae1bd5342e0a081bc
Hirandalpe <65526913+Hirandalpe@users.noreply.github.com>
2021-02-06 22:29:39 +0530

    Creating Daily tips

--- Osumen_TheVirtualFriend/MediSearhPage.xaml.cs
+++ Osumen_TheVirtualFriend/MediSearhPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Data.SqlClient;
 
 namespace Osumen_TheVirtualFriend
 {
@@ -21,6 +22,38 @@ namespace Osumen_TheVirtualFriend
         private void appointmentbutton_Loaded(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Tiptext_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Tiptext_Loaded(object sender, RoutedEventArgs e)
+        {
+            string contip = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename='C: \Users\hdalp\OneDrive - National School of Busness Management\Documents\Osumen.mdf';Integrated Security=True;Connect Timeout=30";
+            using (SqlConnection con = new SqlConnection(contip))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT tip FROM Dailytips Where ID = 2"))
+                {
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        sdr.Read();
+                        Tiptext.Text = sdr.ToString();
+                    }
+                    con.Close();
+
+                         
+                }
+            }
         }
     }
 }
