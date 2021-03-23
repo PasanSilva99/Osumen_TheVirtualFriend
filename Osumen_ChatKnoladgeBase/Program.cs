b886b5be10c1d931e80009b08ec4943f023e3a74
Pasan Anuththara <pasananuththara19@gmail.com>
2021-03-23 09:23:21 +0530

    ChatModel And Serializer

---
+++ Osumen_ChatKnoladgeBase/Program.cs
@@ -0,0 +1,92 @@
+﻿using SimpleNetNlp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Osumen_ChatKnoladgeBase.NLPProcessor;
+using static Osumen_ChatKnoladgeBase.Classifier;
+using Osumen_ChatKnoladgeBase.Trainer;
+
+namespace Osumen_ChatKnoladgeBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("============================================");
+            Console.WriteLine("        Osumen Chat Knoladge Base");
+            Console.WriteLine("            Console Visualizer");
+            Console.WriteLine("============================================");
+            Console.WriteLine();
+
+            while (true)
+            {
+
+                Console.Write("Console Input : User Message Input > ");
+                String inputString = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("*********************************************");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Starting NLP Process");
+
+                NLPProcessor processor = new NLPProcessor();
+
+                Sentence[] sentences = processor.FilterSentences(inputString);
+
+                Word[] ProcessedWords;
+
+
+                foreach (Sentence sentence in sentences)
+                {
+                    ProcessedWords = processor.ProcessSentence(sentence);
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    foreach (Word word in ProcessedWords)
+                    {
+                        Console.Write($"({word.Original}, \'{word.Lemma}\', \'{word.PosTag}\', \"{word.Ner}\") ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine("#############################################");
+                Console.WriteLine("NLP Process Complete");
+
+                Console.WriteLine("*********************************************");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("All Tasks Complete");
+
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+
+                ModelHandler hn = new ModelHandler();
+
+                hn.readTrainData();
+
+                var trainingData = hn.fitDataAsTagPattern();
+
+                hn.SelializeTrainingTAG(trainingData);
+
+
+                Console.Write("Re run? (y/n) > ");
+
+                if (Console.ReadLine() != "y")
+                {
+                    break;
+                }
+
+            }
+
+            Console.WriteLine("============================================");
+            Console.WriteLine("        Osumen Chat Knoladge Base");
+            Console.WriteLine("            Console Visualizer");
+            Console.WriteLine("============================================");
+            Console.WriteLine();
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Osumen_ChatKnoladgeBase/Trainer/ModelHandler.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Newtonsoft.Json;
+
+namespace Osumen_ChatKnoladgeBase.Trainer
+{
+    public class ModelHandler
+    {
+        public class Intent
+        {
+            public String Tag { get; set; }
+            public String[] Patterns { get; set; }
+            public String[] Responces { get; set; }
+        }
+
+        public class IntentTrainingDataTAG
+        {
+            [LoadColumn(0)]
+            public String Tag { get; set; }
+            public String Pattern { get; set; }
+        }
+
+        List<Intent> _intents = new List<Intent>();
+
+        public void saveModel()
+        {
+
+            using (StreamWriter file = File.CreateText(@"D:\intents.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, _intents);
+            }
+        }
+
+        public async void readTrainData()
+        {
+            Console.WriteLine("##########################################");
+            Console.WriteLine("              Reading File ");
+            var jsonString = File.ReadAllText(@"D:\intents.json");
+            trainintents = JsonConvert.DeserializeObject<Intent[]>(jsonString);
+
+            Console.WriteLine("Ser");
+            foreach (var s in trainintents)
+            {
+                Console.WriteLine(s.Tag);
+            }
+            Console.WriteLine("##########################################");
+
+        }
+
+        Intent[] trainintents = null;
+
+        public void TrainModel()
+        {
+            MLContext mLContext = new MLContext();
+            IDataView data = mLContext.Data.LoadFromEnumerable<Intent>(trainintents);
+        }
+
+        public IntentTrainingDataTAG[] fitDataAsTagPattern()
+        {
+            Console.WriteLine("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+");
+            Console.WriteLine("                Loading Chat Model\n");
+
+            List<IntentTrainingDataTAG> intentTrainingDataTAGs = new List<IntentTrainingDataTAG>();
+
+            foreach(var I in trainintents)
+            {
+                Console.Write("{ " + I.Tag + ", (");
+                for(int i = 0; i < I.Patterns.Length; i++)
+                {
+                    Console.Write(I.Patterns[i] + ", ");
+                    intentTrainingDataTAGs.Add(new IntentTrainingDataTAG
+                    {
+                        Tag = I.Tag,
+                        Pattern = I.Patterns[i]
+                    });
+                }
+                Console.Write(" )}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n             Done Chat Model Loading ");
+            Console.WriteLine("+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+_+");
+
+            return intentTrainingDataTAGs.ToArray();
+
+        }
+
+        public void SelializeTrainingTAG(IntentTrainingDataTAG[] ts)
+        {
+            Console.WriteLine("=============================================");
+            Console.WriteLine("             Serializing Tags\n");
+
+            using (StreamWriter file = File.CreateText(@"D:\TrainingData.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, ts);
+            }
+
+            Console.WriteLine("\n               Done Serializing");
+            Console.WriteLine("=============================================");
+
+        }
+
+
+    }
+}
