2cb490baacd124564c25dbfc4086f38f5953754f
Pasan Anuththara <pasananuththara19@gmail.com>
2021-03-28 05:50:11 +0530

    Intents

--- Osumen_TheVirtualFriend/MainPage.xaml.cs
+++ Osumen_TheVirtualFriend/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Osumen_TheVirtualFriend.ChatKnoladgeBase.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -25,7 +26,9 @@ namespace Osumen_TheVirtualFriend
         public MainPage()
         {
             this.InitializeComponent();
-            PageLoader.Navigate(typeof(HomePage));
+           // PageLoader.Navigate(typeof(HomePage));
+            ModelHandler handler = new ModelHandler();
+            var inte = handler.PrepareTrainDataAsync();
         }
         
     }
